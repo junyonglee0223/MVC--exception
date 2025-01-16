0454a79ae889379aa6652b7b6c17d1aeae397573
junyonglee0223 <ljyong3339@gmail.com>
2025-01-16 14:10:21 +0900

    feat: api 예외처리 구현 + servlet getAttributes, setAttributes 분석

---
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,26 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id")String id){
+        if(id.equals("ex")){
+            throw new RuntimeException("wrong user");
+        }
+        return new MemberDto(id, "test-member-"+id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MemberDto{
+        private String memberId;
+        private String name;
+    }
+}
--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,10 +1,19 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -34,6 +43,23 @@ public class ErrorPageController {
         log.info("errorPage 500");
         printErrorInfo(request);
         return "/error-page/500";
+    }
+
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorApi500(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+        log.info("api error page 500");
+
+        Map<String, Object>errorResult = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+
+        errorResult.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        errorResult.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity<>(errorResult, HttpStatus.valueOf(statusCode));
     }
 
     private void printErrorInfo(HttpServletRequest request) {
